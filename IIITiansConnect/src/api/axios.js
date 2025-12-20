af2fbca2cd411f7db3ac9e912f46a3bca73519f1
Ankur <ankurp22singh@gmail.com>
2025-12-20 10:11:25 +0530

    with credentials ko false kiya in frontend src->api->

--- IIITiansConnect/src/api/axios.js
+++ IIITiansConnect/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  withCredentials: false,
 });
 
 api.interceptors.request.use(
